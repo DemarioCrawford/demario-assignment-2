849c4572fb73ec9a35250a233a8e8e0aadfa32ec
DemarioCrawford <Rooneymusic4@gmail.com>
2024-04-13 14:46:31 -0700

    Game done and formatted code

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -9,28 +9,28 @@ public class Assignment2Application {
 //		Instantiate the scanner and random objects
 		Random random = new Random();
 //		Generate random number and number of attempts allowed
-		int randomNumber = random.nextInt(101); 
+		int randomNumber = random.nextInt(101);
 		int guesses = 5;
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Welcome to the guessing game!");
 		System.out.println("You have 5 tries to guess the correct number between 1 and 100.");
-		
+
 		while (guesses > 0) {
-			System.out.println("Attempts left: " + guesses );
+			System.out.println("Attempts left: " + guesses);
 			System.out.println("Pick a number between 1 and 100");
-			int guess = scanner.nextInt(); // scanner will read this 
-			
+			int guess = scanner.nextInt(); // scanner will read this
+
 			if (guess < 1 || guess > 100) {
 				System.out.println("Your guess is not between 1 and 100");
-				continue; //Don't take a way a try
+				continue; // Don't take a way a try
 			}
-			
+
 			if (guess == randomNumber) {
 				System.out.println("You win! You chose the correct number.");
-				return; //exit game
+				return; // exit game
 			}
-			if (guess > randomNumber ) {
+			if (guess > randomNumber) {
 				System.out.println("Pick a lower number.");
 				guesses--;
 			}
@@ -39,10 +39,9 @@ public class Assignment2Application {
 				guesses--;
 			}
 		}
-		
+
 		System.out.println("You have used up all of your attempts. You lose!");
 		System.out.println("The number to guess was: " + randomNumber);
-		
-		
+
 	}
 }
